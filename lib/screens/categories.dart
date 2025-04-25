2b931ba7829e0261b4005ebebf2ea4e925d79d65
Cristian Murillo <cristianmgm7@gmail.com>
2025-04-25 14:59:43 +0200

    Initialize Flutter project structure with essential files and configurations for Android, iOS, macOS, Linux, and Windows platforms. Added .gitignore files, project metadata, and basic app structure including main Dart files, assets, and platform-specific configurations.

---
+++ lib/screens/categories.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categories')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()----> another way to do it
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
