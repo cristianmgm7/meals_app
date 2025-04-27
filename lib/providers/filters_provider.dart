5a1d35256ad88f2ec4be9d123c3ad892af6bc4b8
Cristian Murillo <cristianmgm7@gmail.com>
2025-04-27 18:27:37 +0200

    Add filteredMealsProvider to manage meal filtering logic
    - Introduced filteredMealsProvider to encapsulate filtering logic based on active filters.
    - Updated TabsScreen to utilize filteredMealsProvider for displaying available meals.
    - Removed direct filter logic from TabsScreen to enhance code maintainability.

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/models/filter.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
   FiltersNotifier()
@@ -24,3 +25,25 @@ final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
       (ref) => FiltersNotifier(),
     );
+
+// --> This is the provider for the filtered meals
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:meals_app/models/filter.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
-import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/providers/favorites.provider.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 
@@ -29,24 +27,8 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals =
-        meals.where((meal) {
-          if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-            return false;
-          }
-          if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-            return false;
-          }
-          if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-            return false;
-          }
-          if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-            return false;
-          }
-          return true;
-        }).toList();
+    // --> This is the provider for the filtered meals
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(meals: availableMeals);
     var activePageTitle = 'Categories';
@@ -59,11 +41,15 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
     void selectScreen(String identifier) async {
       Navigator.of(context).pop();
-
       if (identifier == 'filters') {
         await Navigator.of(
           context,
         ).push(MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+      }
+      if (identifier == 'meals') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (ctx) => const TabsScreen()),
+        );
       }
     }
 
